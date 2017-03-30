19a11d3df62a38714b1b94ddd756e95cb42eaa17
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-03-30 16:51:42 +0000

    trying out more things to tie ground movement to frames

--- client/app/scripts/ground.js
+++ client/app/scripts/ground.js
@@ -6,6 +6,7 @@ window.Ground = (function() {
 	var HEIGHT = 12.8;
     var SPEED = 30;
     var moved = false;
+    var move = 0;
 
     var Ground = function(el, game) {
         this.el = el;
@@ -17,46 +18,30 @@ window.Ground = (function() {
         this.posX = 0;
     };
 
-    Ground.prototype.moveGround = function() {
+    /*Ground.prototype.moveGround = function() {
         this.posX -= WIDTH;
 
         this.el.css('transform', 'translate(' + this.posX + 'em, 0em');
 
         this.reset();
-    }
+    }*/
 
     Ground.prototype.stopMove = function() {
         document.getElementById("Ground").style.animation = "none";
-    }
+    };
 
-    Ground.prototype.onFrame = function(delta) {
-        //console.log(delta);
-
-        //this.el.css('animation', 'groundmove, '+ delta +'s, linear, infinite');
-        //this.el.style.animation = "groundmove " + delta + "s linear infinite";
-        //document.getElementById("Ground").style.animation = "groundmove "+delta*10+"s linear infinite";
+    Ground.prototype.onFrame = function(delta) {        
         document.getElementById("Ground").style.animation = "groundmove 0.12s linear infinite";
-        /*if(moved) {
-            this.el.css('transform', 'none');
-            moved = false;
-        }
-        else {
-            this.el.css('transform', 'translate(-3.7em, 0em');
-            moved = true;
-        }*/
+        
+        //this.move = delta * SPEED * 10;
+        //document.getElementById("Ground").style.animation = "groundmove"+move+"s linear infinite";
+        /*this.posX -= delta * SPEED;
+        console.log(this.posX);
 
-        //this.posX -= delta * SPEED + 3.7;
-        //this.posX -= (delta * 100)/2;
-        //this.posX -= 37;
-        //console.log(this.posX);
-        /*if (this.posX >= -37) {
-            console.log("found number");
-            this.posX = 0;
-        }*/
+        this.checkOutOfBounds();
 
-        //this.el.css('transform', 'translate(-37em, 0em');
+        this.el.css('transform', 'translate(' + this.posX + 'em, 0 em)');*/
 
-        //this.reset();
         
         //var id = setInterval(frame, delta);
         /*function frame() {
@@ -69,14 +54,16 @@ window.Ground = (function() {
                 this.el.css('left', + this.posX + 'em');
             }
 
-        }*/
-        //this.pos.x -= delta * SPEED;
-
-        //this.el.css('transform', 'translate(' + this.pos.x + 'em, 0 em)');
-
         //this.el.css('transform', 'translateX(' + this.pos.x + 'em)');
-        //this.el.css('animation', 'groundmove ' + delta*30 + 's linear infinite');
+        //this.el.css('animation', 'groundmove ' + delta*30 + 's linear infinite');*/
     };
+
+    /*Ground.prototype.checkOutOfBounds = function() {
+        if(this.posX < -37) {
+            console.log("PosX less");
+            this.posX = 0;
+        }
+    };*/
 
     return Ground;
 
