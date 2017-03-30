2cb9fb8e555c61ae53aa7a5866e7887f3e0b527a
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-03-30 16:23:39 +0000

    all kinds of animation tests, none work well

--- client/app/scripts/ground.js
+++ client/app/scripts/ground.js
@@ -5,6 +5,7 @@ window.Ground = (function() {
     var WIDTH = 3.7;
 	var HEIGHT = 12.8;
     var SPEED = 30;
+    var moved = false;
 
     var Ground = function(el, game) {
         this.el = el;
@@ -16,12 +17,42 @@ window.Ground = (function() {
         this.posX = 0;
     };
 
-    Ground.prototype.onFrame = function(delta) {
-        //this.posX -= delta * SPEED + 3.7;
-        this.posX -= (delta * 100)/2;
+    Ground.prototype.moveGround = function() {
+        this.posX -= WIDTH;
 
         this.el.css('transform', 'translate(' + this.posX + 'em, 0em');
+
         this.reset();
+    }
+
+    Ground.prototype.onFrame = function(delta) {
+        //console.log(delta);
+
+        //this.el.css('animation', 'groundmove, '+ delta +'s, linear, infinite');
+        //this.el.style.animation = "groundmove " + delta + "s linear infinite";
+        //document.getElementById("Ground").style.animation = "groundmove "+delta*10+"s linear infinite";
+        /*if(moved) {
+            this.el.css('transform', 'none');
+            moved = false;
+        }
+        else {
+            this.el.css('transform', 'translate(-3.7em, 0em');
+            moved = true;
+        }*/
+
+        //this.posX -= delta * SPEED + 3.7;
+        //this.posX -= (delta * 100)/2;
+        //this.posX -= 37;
+        //console.log(this.posX);
+        /*if (this.posX >= -37) {
+            console.log("found number");
+            this.posX = 0;
+        }*/
+
+        //this.el.css('transform', 'translate(-37em, 0em');
+
+        //this.reset();
+        
         //var id = setInterval(frame, delta);
         /*function frame() {
             if (this.posX === -37)
