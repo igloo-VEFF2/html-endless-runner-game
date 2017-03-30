06d0f1f5dce6ea7a3558c83c0dcc3f9a99221424
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-03-30 03:07:12 +0000

    messing around with the ground

--- client/app/scripts/ground.js
+++ client/app/scripts/ground.js
@@ -4,19 +4,26 @@ window.Ground = (function() {
     var TOTALWIDTH = 106.1; // image is 37 pixels wide, width of gamecanvas is 1024 pixels
     var WIDTH = 3.7;
 	var HEIGHT = 12.8;
+    var SPEED = 30;
 
     var Ground = function(el, game) {
         this.el = el;
         this.game = game;
-        this.pos.x = 0;
+        this.pos = { x: 0, y: 0 };
     };
 
     Ground.prototype.reset = function() {
         this.pos.x = 0;
+        this.pox.y = 0;
     };
 
     Ground.prototype.onFrame = function(delta) {
-        this.el.css('transform', 'translateX('+ -this.WIDTH +'em)');
+        this.pos.x -= delta * SPEED;
+
+        this.el.css('transform', 'translate(' + this.pos.x + 'em, 0 em)');
+
+        //this.el.css('transform', 'translateX(' + this.pos.x + 'em)');
+        //this.el.css('animation', 'groundmove ' + delta*30 + 's linear infinite');
     };
 
     return Ground;
